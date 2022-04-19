abcee42eb6433eb544abeb0f60cd9418095e9f9a
Benjamin Smith <ben.smith.developer@gmail.com>
2022-04-19 13:09:02 -0700

    more cleanup

--- modules/cloudposse-deepmerge/versions.tf
+++ modules/cloudposse-deepmerge/versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
     template = {
       source  = "hashicorp/template"
       version = ">= 2.2"
