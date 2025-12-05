ba174b2f2afff54340e4fe269addc72efd9d0fa2
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-12-06 00:36:53 +0300

    Update versions.tf

--- modules/assert/versions.tf
+++ modules/assert/versions.tf
@@ -2,17 +2,9 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    template = {
-      source  = "cloudposse/template"
-      version = ">= 2.2"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 2.0"
-    }
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 0.3.0"
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 2.3.2"
     }
   }
 }
